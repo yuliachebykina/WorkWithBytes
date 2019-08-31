31d9f0841118e7f3edc915635ded4acd3bbb23e2
Yulia Chebykina <YuliaChebykina@gmail.com>
2019-08-31 00:21:23 -0700

    Initial commit

--- src/info/sjd/ApplicationRunner.java
+++ src/info/sjd/ApplicationRunner.java
@@ -4,16 +4,11 @@ import info.sjd.service.FileService;
 
 public class ApplicationRunner {
 
+    public static void main(String[] args){
 
-        public static void main(String[] args) {
 
-
-            // reading bytes from file1
-            FileService.ReadingFromFile("file1.txt");
-
-            //writing bytes to file2
-            FileService.WritingToFile("file2.txt");
-
+            FileService.moveFileToNewDirectory("dog.jpg", "dog_new.jpg","files_new");
+            FileService.moveFileToNewDirectory("cat.jpg", "cat_new.jpg","files_new");
         }
-    }
+}
 
--- src/info/sjd/service/FileService.java
+++ src/info/sjd/service/FileService.java
@@ -1,46 +1,61 @@
 package info.sjd.service;
 
 import java.io.*;
+import java.nio.file.Files;
+
 
 public class FileService {
     private static final String MAIN_DIR = System.getProperty("user.dir");
     private static final String FILE_SEP = System.getProperty("file.separator");
     private static final String FILE_DIR = MAIN_DIR + FILE_SEP + "files";
+    private static final String FILE_DIR_NEW = MAIN_DIR + FILE_SEP + "files_new";
 
 
-    //create file
     private static void checkDir() {
         File file = new File(FILE_DIR);
-
         if (!file.exists()) {
             file.mkdir();
         }
     }
 
-    //reading bytes from file1
 
-    public byte[] ReadingFromFile(String fileName) {
-        try (FileReader reader = new FileReader(FILE_DIR + FILE_SEP + fileName);
-             BufferedReader br = new BufferedReader(reader)) {
+    public static byte[] getBytesFromFile(String fileName){
+        File file = new File(FILE_DIR + FILE_SEP + fileName);
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
 
-            // read line by line
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-
-        } catch (IOException e)
-
-
-        //writing bytes to file2
-
-        public byte[] WritingToFile(String newfileName) {
-            checkDir();
-
-            try (FileWriter fileWriter = new FileWriter(FILE_DIR + FILE_SEP + newfileName)) {
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public  static void writeBytesToFile(byte[] bytes, String fileName){
+        checkDir();
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_DIR_NEW + FILE_SEP + fileName)) {
+            fileOutputStream.write(bytes);
+            fileOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    public static void copyFile(String fromFile, String toFile){
+        byte[] bytes = getBytesFromFile(fromFile);
+        writeBytesToFile(bytes, toFile);
+    }
+
+    public static void moveFileToNewDirectory(String fromFile, String toFile, String new_directory){
+        byte[] bytes = getBytesFromFile(fromFile);
+
+        File file = new File(FILE_DIR + FILE_SEP + fromFile);
+        //file.delete();
+
+    }
 }
+
+
+
+
+
+
